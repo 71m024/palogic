7d16a6f1adcea964f8cbbe4db95aef5225685283
Timo Bühlmann <timo.buehlmann@gmail.com>
2014-07-09 11:27:52 +0200

    corrected wrong upload-path

--- Symfony/src/Boxenmieten/AppBundle/Entity/Image.php
+++ Symfony/src/Boxenmieten/AppBundle/Entity/Image.php
@@ -95,7 +95,7 @@ class Image
     {
         // the absolute directory path where uploaded
         // documents should be saved
-        return __DIR__.'/../../../../../httpdocs/'.$this->getUploadDir();
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
     }
 
     protected function getUploadDir()
