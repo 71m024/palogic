c0796064590088a5b45c73761728a5903572ec5d
Timo Bühlmann <timo.buehlmann@gmail.com>
2014-08-30 10:21:28 +0200

    improved profile-page

--- src/PaLogic/Bundle/UserBundle/Entity/PaUser.php
+++ src/PaLogic/Bundle/UserBundle/Entity/PaUser.php
@@ -24,6 +24,12 @@ class PaUser extends BaseUser
      * @ORM\OneToMany(targetEntity="PaLogic\Bundle\DjBundle\Entity\Dj", mappedBy="owner")
      */
     private $djs;
+    
+    /**
+     *
+     * @ORM\OneToMany(targetEntity="PaLogic\Bundle\ImageBundle\Entity\Image", mappedBy="owner")
+     */
+    private $images;
 
     public function __construct()
     {
@@ -72,5 +78,38 @@ class PaUser extends BaseUser
     public function getDjs()
     {
         return $this->djs;
+    }
+
+    /**
+     * Add images
+     *
+     * @param \PaLogic\Bundle\ImageBundle\Entity\Image $images
+     * @return PaUser
+     */
+    public function addImage(\PaLogic\Bundle\ImageBundle\Entity\Image $images)
+    {
+        $this->images[] = $images;
+
+        return $this;
+    }
+
+    /**
+     * Remove images
+     *
+     * @param \PaLogic\Bundle\ImageBundle\Entity\Image $images
+     */
+    public function removeImage(\PaLogic\Bundle\ImageBundle\Entity\Image $images)
+    {
+        $this->images->removeElement($images);
+    }
+
+    /**
+     * Get images
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getImages()
+    {
+        return $this->images;
     }
 }
