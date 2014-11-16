b89c66d2733efed142f9146c428145f57c3e2ce1
Timo Bühlmann <timo.buehlmann@gmail.com>
2014-11-16 19:19:51 +0100

    updated Logo, removed siteswitcher from BM and updated the banner

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -60,4 +60,10 @@ class AppKernel extends BaseKernel
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    public function init()
+    {
+        date_default_timezone_set( 'Europe/Berlin' );
+        parent::init();
+    }
 }
